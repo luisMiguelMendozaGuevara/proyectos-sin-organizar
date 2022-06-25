d7c725a0c00137e14e04a4f809202f39d586122d
Luis Miguel Mendoza Guevara <90010768+luisMiguelMendozaGuevara@users.noreply.github.com>
2022-06-25 23:07:24 +0000

    primer commit1

---
+++ Components/contador/index.js
@@ -0,0 +1,26 @@
+var numero = 0
+
+document.getElementById("numero1").innerHTML = "<h1>" + numero + "</h1>"
+
+
+function aumentar() {
+
+    numero += 1
+
+    document.getElementById("numero1").innerHTML = "<h1>" + numero + "</h1>"
+
+}
+function reiniciar() {
+
+    numero = 0
+
+    document.getElementById("numero1").innerHTML = "<h1>" + numero + "</h1>"
+
+}
+function decrementar() {
+
+    numero += -1
+
+    document.getElementById("numero1").innerHTML = "<h1>" + numero + "</h1>"
+
+}
